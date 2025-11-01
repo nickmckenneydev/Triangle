2b5b83124c1cf9a06ee8c8bdd3b2e96dc2a15ede
nickmckenneydev <nick.mckenney.dev@gmail.com>
2025-11-01 15:27:59 -0400

    removed try catch

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,20 @@
 import { Renderer } from "./renderer";
 
-
 async function main(){
     const canvas:HTMLCanvasElement = document.getElementById("gfx-main") as HTMLCanvasElement;
     if(!canvas){
         console.log("Canvas Error failed to load");
         return 1;
     }
-    try {
-        const renderer = await Renderer.create(canvas);
-        //render loop
-        function gameLoop() {
-            
-            renderer.render();//Call renderer public render method
+  
+    const renderer = await Renderer.create(canvas);
+  
+    function gameLoop() {
+            requestAnimationFrame(gameLoop);//Request the next frame from the browser. Schdule work first, then do rendering
 
-            
-            requestAnimationFrame(gameLoop);//Request the next frame from the browser
+            renderer.render();//Call renderer public render method
         }
       
-        requestAnimationFrame(gameLoop);//restart
-        
-    } catch(error) {
-        console.error("WOMP WOMP",error);
-    }
+    requestAnimationFrame(gameLoop);//Entry point into loop. Only called once. gameLoop is the call back function
 }
 main();
