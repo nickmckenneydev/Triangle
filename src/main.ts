010603769a1e3ec8af63859242888341c8f2e8a3
nickmckenneydev <nick.mckenney.dev@gmail.com>
2025-11-01 15:43:52 -0400

    Made changes to package json to add prettier

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,20 @@
-import { Renderer } from "./renderer";
+import { Renderer } from './renderer';
 
-async function main(){
-    const canvas:HTMLCanvasElement = document.getElementById("gfx-main") as HTMLCanvasElement;
-    if(!canvas){
-        console.log("Canvas Error failed to load");
-        return 1;
-    }
-  
-    const renderer = await Renderer.create(canvas);
-  
-    function gameLoop() {
-            requestAnimationFrame(gameLoop);//Request the next frame from the browser. Schdule work first, then do rendering
+async function main() {
+	const canvas: HTMLCanvasElement = document.getElementById('gfx-main') as HTMLCanvasElement;
+	if (!canvas) {
+		console.log('Canvas Error failed to load');
+		return 1;
+	}
 
-            renderer.render();//Call renderer public render method
-        }
-      
-    requestAnimationFrame(gameLoop);//Entry point into loop. Only called once. gameLoop is the call back function
+	const renderer = await Renderer.create(canvas);
+
+	function gameLoop() {
+		requestAnimationFrame(gameLoop); //Request the next frame from the browser. Schdule work first, then do rendering
+
+		renderer.render(); //Call renderer public render method
+	}
+
+	requestAnimationFrame(gameLoop); //Entry point into loop. Only called once. gameLoop is the call back function
 }
 main();
