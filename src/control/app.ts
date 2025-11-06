647dc3e75f952bc35b06068d6a0a86cdfb429cf5
nickmckenneydev <nick.mckenney.dev@gmail.com>
2025-11-05 20:41:25 -0500

    added keypress event

--- src/control/app.ts
+++ src/control/app.ts
@@ -1,15 +1,25 @@
 import { Renderer } from '../view/renderer';
 import { Scene } from '../model/scence';
+import { event } from 'jquery';
 
 export class App {
 	canvas: HTMLCanvasElement;
 	renderer: Renderer;
 	scence: Scene;
 
+	keyLabel: HTMLElement;
+	mouseXLabel: HTMLElement;
+	mouseYLabel: HTMLElement;
+
 	constructor(canvas: HTMLCanvasElement) {
 		this.canvas = canvas;
 		this.renderer = new Renderer(canvas);
 		this.scence = new Scene();
+
+		this.keyLabel = <HTMLElement>document.getElementById('key-label');
+		$(document).on('keypress', (event) => {
+			this.handle_keypress(event);
+		});
 	}
 
 	async initialize() {
@@ -25,4 +35,8 @@ export class App {
 			requestAnimationFrame(this.run);
 		}
 	};
+
+	handle_keypress(event: JQuery.KeyPressEvent) {
+		this.keyLabel.innerText = event.code;
+	}
 }
