f4a420eba72d1c56a64370c0edd70c9e30992764
nickmckenneydev <nick.mckenney.dev@gmail.com>
2025-11-08 19:21:51 -0500

    Grab elements of matrix

--- src/model/scene.ts
+++ src/model/scene.ts
@@ -1,13 +1,29 @@
 import { Triangle } from './triangle';
 import { Camera } from './camera';
-import { vec3 } from 'gl-matrix';
+import { vec3, mat4 } from 'gl-matrix';
 export class Scene {
 	triangles: Triangle[];
 	player: Camera;
+	object_data: Float32Array;
+	triangle_count: number;
 
 	constructor() {
 		this.triangles = [];
-		this.triangles.push(new Triangle([2, 0, 0], 0));
+		this.object_data = new Float32Array(16 * 1024);
+		this.triangle_count = 0;
+
+		var i: number = 0;
+		for (var y: number = -5; y < 5; y++) {
+			this.triangles.push(new Triangle([2, y, 0], 0));
+
+			var blank_matrix = mat4.create();
+			for (var j: number = 0; j < 16; j++) {
+				this.object_data[16 * i + j] = <number>blank_matrix[j]; //Grabs all elements of matrix
+			}
+			i++;
+			this.triangle_count++;
+		}
+
 		this.player = new Camera([-2, 0, 0.5], 0, 0);
 	}
 
